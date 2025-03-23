6c7ffd8f975197546b1c6eb79288d975e10b16be
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-23 14:21:13 +0600

    Update FirstClass.java

--- First Class/src/first/pkgclass/FirstClass.java
+++ First Class/src/first/pkgclass/FirstClass.java
@@ -14,7 +14,7 @@ public class FirstClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        System.out.println("Hello, World!");
     }
     
 }
