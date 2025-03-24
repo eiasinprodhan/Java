3c7205c791ede5543247d1adb90a35d78a6b9e26
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-24 14:13:15 +0600

    Changed

---
+++ FirstClass/src/firstclass/FirstClass.java
@@ -0,0 +1,30 @@
+package firstclass;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        byte a = 120;
+        short b = 32670;
+        int c = 15986548;
+        long d = 1548648l;
+        
+        float e = 4654654.41654f;
+        double f = 4564654654654646454.654654987984;
+        
+        char g = 'A';
+        char h = '\u0041';
+        
+        String userName = "Eiasin Prodhan";
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(userName);
+    }
+    
+}
