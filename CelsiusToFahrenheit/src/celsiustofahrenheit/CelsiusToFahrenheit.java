99dbc67ba4c3ca44eb4109e9350d3348030da5df
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-24 22:50:31 +0600

    Update CelsiusToFahrenheit.java

--- CelsiusToFahrenheit/src/celsiustofahrenheit/CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit/src/celsiustofahrenheit/CelsiusToFahrenheit.java
@@ -6,10 +6,14 @@ public class CelsiusToFahrenheit {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter Celsius: ");
-        int input = scan.nextInt();
-        int result = input*9/5+32;
-        System.out.println(input + " Celsius is " + result + " Fahrenheit");
-    }
+        System.out.print("Enter Celsius (Integer Number Only): ");
+        if (scan.hasNextInt()) {
+            int input = scan.nextInt();
+            int result = input * 9 / 5 + 32;
+            System.out.println(input + " Celsius is " + result + " Fahrenheit");
+        } else {
+            System.out.println("Please enter a integer number");
+        }
 
+    }
 }
