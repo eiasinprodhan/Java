c044923e3bce9abe4f9f619a67b2514e2e8ff444
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-24 16:28:21 +0600

    Changed

---
+++ FactorialNumber/src/factorialnumber/FactorialNumber.java
@@ -0,0 +1,18 @@
+package factorialnumber;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int input = scan.nextInt();
+        int result = 1;
+        for(int i = 1; i<=input; i++){
+            result*=i;
+        }
+        System.out.println("Your Result is: " + result);
+        }
+    
+}
