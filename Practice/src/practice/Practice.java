d3cef0cc00601418d8d63ff49ca7cf8a41723ffc
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-05 16:57:38 +0600

    Changed

---
+++ Practice/src/practice/Practice.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = a++;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b);
+        
+    }
+}
