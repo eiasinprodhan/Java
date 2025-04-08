61fc820770c13941119d76c054eb0055625a060a
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-08 15:17:16 +0600

    Changed

--- NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -7,24 +7,24 @@ public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int rand = (int) (Math.random() * 10 + 1);
+        int rand = (int) (Math.random() * 10);
 
-        int chance = 0;
+        int count = 0; //1, 
 
-        while (chance < 3) {
-            System.out.print("Guess A Number In 1-9 (You Will Get 3 Chances): ");
+        while (count < 3) {
+            System.out.print("Guess a number between 1-10 (You have only 3 chances to guess): ");
             int input = scan.nextInt();
             
             if (input == rand) {
-                System.out.println("Hooray! You Win");
+                System.out.println("Hooray! you win");
                 break;
             } else {
-                chance++;
-                System.out.print("You Lose");
+                count++;
+                System.out.print("Alas! you lose");
                 if (input > rand) {
-                    System.out.println(" And Your Input is greater than Random Number");
+                    System.out.println(" and your guess is greater than the Number");
                 } else {
-                    System.out.println(" And Your Input is less than Random Number");
+                    System.out.println(" and your guess is less than the number");
                 }
             }
         }
