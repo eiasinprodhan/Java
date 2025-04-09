5f6a06683e107786b40d06fc8db65ebe9eba5895
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-09 23:51:28 +0600

    Update NumberGuessingGame.java

--- NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -9,27 +9,31 @@ public class NumberGuessingGame {
 
         int rand = (int) (Math.random() * 10);
 
-        int count = 0;
+        int count = 1;
 
-        while (count < 3) {
+        while (count <= 3) {
             System.out.print("Guess a number between 1-10 (You have only 3 chances to guess): ");
             int input = scan.nextInt();
             
             if (input == rand) {
                 System.out.println("Hooray! you win");
                 break;
-            } else {
-                count++;
-                System.out.print("Alas! you lose");
-                if (input > rand) {
-                    System.out.println(" and you guess too high");
-                } else {
-                    System.out.println(" and you guess too low");
-                }
             }
+            else if(input>rand && count<=2){
+                System.err.println("Too Higher");
+            }
+            else if(input<rand && count<=2){
+                System.err.println("Too Lower");
+            }
+            
+            if(count==3){
+                System.err.println("Alas! You Lose");
+            }
+            
+            count++;
         }
         
-        System.out.println("Correct number is: " + rand);
+        System.err.println("Correct number is: " + rand);
     }
 
 }
