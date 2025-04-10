288c701c52d13225b6d7606c6325492e900388d2
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-10 18:13:32 +0600

    Update Methods.java

--- Methods/src/methods/Methods.java
+++ Methods/src/methods/Methods.java
@@ -5,21 +5,27 @@ import java.util.Scanner;
 public class Methods {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
-        
+
+        //User Input And Messages
         System.out.print("Enter your number: ");
         int input = scan.nextInt();
-        
+
+        //Declaring Methods In Variables
         String prime = findPrimeNumber(input);
-        System.out.println(prime);
-        
+        String oddEven = findOddEven(input);
         double circle = findAreaOfCircle(input);
+
+        //Printing Method's Variable
+        System.out.println(prime);
+        System.out.println(oddEven);
         System.out.println("Area Of Circle: " + circle);
     }
 
+    //Function To Find Prime Number
     public static String findPrimeNumber(int input) {
-        
+
         String result = "";
         int count = 0;
         for (int i = 1; i <= input; i++) {
@@ -27,20 +33,34 @@ public class Methods {
                 count++;
             }
         }
-        
+
         if (count == 2) {
-            result =  input + " is a prime number.";
+            result = input + " is a prime number.";
         } else {
             result = input + " isn't a prime number.";
         }
         return result;
     }
-    
+
+    //Function To Calculate Area Of Circle
     public static double findAreaOfCircle(int input) {
-        
+
         double result = (double) (Math.PI * Math.pow(input, 2));
-        
+
         return result;
     }
-    
+
+    //Function To Find Odd Or Even Number
+    public static String findOddEven(int input) {
+        String result = "";
+
+        if (input % 2 == 0) {
+            result = input + " is even number.";
+        } else {
+            result = input + " is odd number.";
+        }
+
+        return result;
+    }
+
 }
