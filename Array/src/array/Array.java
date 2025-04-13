3fc61f067658d4b0393e5ac806cf3cae2a2a0333
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-13 18:07:19 +0600

    Update Array.java

--- Array/src/array/Array.java
+++ Array/src/array/Array.java
@@ -1,21 +1,35 @@
 package array;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] names = new String[15];
-
-        for (int i = 0; i < 15; i++) {
-            String userInput = scan.next();
-            names[1] = userInput;
+        System.out.print("How many students do you have?: ");
+        int arraySize = scan.nextInt();
+        
+        String[] names = new String[arraySize];
+        int[] marks = new int[arraySize];
+        
+        String result = "";
+        
+        for(int i=0; i<arraySize; i++){
+            System.out.print("No. " + (int)(i+1) + " student's name: ");
+            String name = scan.next();
+            
+            System.out.print("No. " + (int)(i+1) + " student's mark: ");
+            int mark = scan.nextInt();
+            
+            System.out.println("-------------------------------------");
+            
+            names[i] = name;
+            marks[i] = mark;
+            
+            result+=i+1 + ". "+names[i] + " got " + marks[i] + " number \n"; 
         }
         
-        System.out.println(Arrays.toString(names));
+        System.out.println(result);
         
     }
-
 }
