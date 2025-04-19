8986163557b2cb49f88ed6b49bb2ac9fb88e5669
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-19 19:06:38 +0600

    Changed

--- OOP/src/oop/OOP.java
+++ OOP/src/oop/OOP.java
@@ -2,20 +2,24 @@ package oop;
 
 import java.util.Scanner;
 import oop.tclass.Circle;
+import oop.tclass.Student;
 
 public class OOP {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your radius: ");
-        double userInput = scan.nextDouble();
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter your radius: ");
+//        double userInput = scan.nextDouble();
+//        
+//        Circle circle = new Circle();
+//        
+//        circle.setRadius(userInput);
+//        
+//        System.out.println("Area Of Circle: " + circle.getArea());
+//        System.out.println("Perimeter Of Circle: " + circle.getPerimeter());
         
-        Circle circle = new Circle();
-        
-        circle.setRadius(userInput);
-        
-        System.out.println("Area Of Circle: " + circle.getArea());
-        System.out.println("Perimeter Of Circle: " + circle.getPerimeter());
+        Student s = new Student("Eiasin Prodhan", "eiasinprodhan@gmail.com", 1, 100, "JEE");
+        s.getInfo();
     }
 }
     
---
+++ OOP/src/oop/tclass/Student.java
@@ -0,0 +1,70 @@
+package oop.tclass;
+
+public class Student {
+    String name;
+    String email;
+    int roll;
+    int mark;
+    String subject;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, int roll, int mark, String subject) {
+        this.name = name;
+        this.email = email;
+        this.roll = roll;
+        this.mark = mark;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+    
+    
+    public void getInfo(){
+        System.out.println("Student's Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Roll: " + roll);
+        System.out.println("Mark: " + mark);
+        System.out.println("Subject: " + subject);
+    }
+}
