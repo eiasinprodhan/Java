9b410108cf686dc2ef2291647360759f8cbcfc1e
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-04-19 15:39:49 +0600

    Changed

---
+++ BubbleSort/src/bubblesort/BubbleSort.java
@@ -0,0 +1,10 @@
+
+package bubblesort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- TwoDimensionalArray/src/twodimensionalarray/TwoDimensionalArray.java
+++ TwoDimensionalArray/src/twodimensionalarray/TwoDimensionalArray.java
@@ -6,28 +6,36 @@ import java.util.Scanner;
 public class TwoDimensionalArray {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("Enter your row limit: ");
         int rowSize = scan.nextInt();
-        
+
         System.out.print("Enter your column limit: ");
         int columnSize = scan.nextInt();
-        
+
         int[][] myArrays = new int[rowSize][columnSize];
-        
-        for(int row = 0; row < myArrays.length; row++){
-            
-            for(int column = 0; column < myArrays[row].length; column++){
-                System.out.print("Enter value for row " + (row+1) + " x column " + (column+1) + ": ");
+
+        for (int row = 0; row < myArrays.length; row++) {
+
+            for (int column = 0; column < myArrays[row].length; column++) {
+                System.out.print("Enter value for row " + (row + 1) + " x column " + (column + 1) + ": ");
                 int userInput = scan.nextInt();
                 myArrays[row][column] = userInput;
             }
         }
-        
-        System.out.println("Your multidimensional array is: " + Arrays.deepToString(myArrays));
-        
+
+        System.out.println("Your array is: ");
+
+        for (int row = 0; row < myArrays.length; row++) {
+
+            for (int column = 0; column < myArrays[row].length; column++) {
+                System.out.print(myArrays[row][column] + " ");
+            }
+            System.out.println("");
+        }
+
     }
-    
+
 }
