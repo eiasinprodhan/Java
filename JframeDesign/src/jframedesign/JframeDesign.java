44c5b47bd3b11cc361eb0ce79f58508b2748bcbc
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-05-17 14:19:13 +0600

    Added

---
+++ JframeDesign/src/jframedesign/JframeDesign.java
@@ -0,0 +1,12 @@
+package jframedesign;
+
+import jframedesign.view.JframeDesignAddView;
+
+public class JframeDesign {
+
+    public static void main(String[] args) {
+        JframeDesignAddView jf = new JframeDesignAddView();
+        jf.setVisible(true);
+    }
+    
+}
---
+++ JframeDesign/src/jframedesign/util/DBUtil.java
@@ -0,0 +1,26 @@
+
+package jframedesign.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBUtil {
+    private Connection connection;
+    private final String url = "jdbc:mysql://localhost:3307/student";
+    private final String user = "root";
+    private final String password = "12345";
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    
+    public Connection getConnection(){
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+}
