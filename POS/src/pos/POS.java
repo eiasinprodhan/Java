fd3255e1b6196dbb09819ae6eb9363054802a5d8
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-05-18 13:43:43 +0600

    Changed

---
+++ POS/src/pos/POS.java
@@ -0,0 +1,12 @@
+package pos;
+
+import view.POSView;
+
+public class POS {
+
+    public static void main(String[] args) {
+        POSView pv = new POSView();
+        pv.setVisible(true);
+    }
+    
+}
