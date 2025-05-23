82f73a69a81a17d4bf95fca8eaf31e0dc7128859
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-05-23 07:29:25 +0300

    Added

--- POS/src/view/POSView.java
+++ POS/src/view/POSView.java
@@ -103,6 +103,7 @@ public class POSView extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1200, 800));
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(0, 102, 102));
