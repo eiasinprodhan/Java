24f36abccc0faf5d5c97c59a7c6f1a11c5b8e0ef
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-05-25 16:06:17 +0600

    Added

---
+++ Website Copier/src/website/copier/WebsiteCopier.java
@@ -0,0 +1,12 @@
+package website.copier;
+
+import view.WebsiteCopierView;
+
+public class WebsiteCopier {
+
+    public static void main(String[] args) {
+        WebsiteCopierView wc = new WebsiteCopierView();
+       wc.setVisible(true);
+    }
+    
+}
